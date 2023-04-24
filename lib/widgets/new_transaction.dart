f7e7e1e6ed9197a8d2b88b964e300ce9a5fe587b
georgeLabrysGroup <106515238+georgeLabrysGroup@users.noreply.github.com>
2023-04-24 15:41:23 +0100

    method for submiting fixed decimal

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -8,6 +8,18 @@ class NewTransaction extends StatelessWidget {
 
   NewTransaction(this.addTx);
 
+  void submitData() {
+    final enteredTitle = titleController.text;
+    final enteredAmount = double.parse(amountController.text);
+    if (enteredTitle.isEmpty || enteredAmount <= 0) {
+      return;
+    }
+    addTx(
+      enteredTitle,
+      enteredAmount,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -27,6 +39,8 @@ class NewTransaction extends StatelessWidget {
             TextField(
               decoration: InputDecoration(labelText: 'Amount'),
               controller: amountController,
+              keyboardType: TextInputType.number,
+              onSubmitted: (_) => submitData(),
               // onChanged: (val) {
               //   amountInput = val;
               // },
@@ -34,9 +48,7 @@ class NewTransaction extends StatelessWidget {
             ElevatedButton(
               child: Text('Add Transaction'),
               //textColor: Colors.purple,
-              onPressed: () {
-                addTx(titleController.text, double.parse(amountController.text));
-              },
+              onPressed: submitData,
             ),
           ],
         ),
